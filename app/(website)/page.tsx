3bcb73b29191b8896b54272b718f1e36504611ea
UnclePacky <claudeaoun@gmail.com>
2024-09-15 18:03:25 +0300

    home page

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function HomePage() {
   return (
     <div>
-      Coming soon
-      {/* <h1 className="text-4xl uppercase">Coming Soon</h1> */}
+      <h1 className="text-4xl uppercase">Coming Soon</h1>
     </div>
   );
 }
